caf12145c3371fe01a96483b93244f54e88260cc
GitHub Action <action@github.com>
2021-03-13 22:37:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/flaviokreis/codeandroidstudiotemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.flaviokreis.codeandroidstudiotemplate.services
+
+import com.github.flaviokreis.codeandroidstudiotemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
